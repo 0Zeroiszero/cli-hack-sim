5b5304baf0e0b2ca52fd5189383a297161149c0d
irfankurniawan25 <irfankurniawan25pku@gmail.com>
2026-05-28 21:50:13 +0700

    update searching

--- DSA/searching.py
+++ DSA/searching.py
@@ -1,29 +1,39 @@
+from server import server_list
+
 class SearchingServer:
     def __init__(self):
         pass
     
-    # fungsi mencari server berdasarkan nama server menggunakan binary search
-    def cari_server(self, data, target):
+    # fungsi mencari server berdasarkan ip menggunakan binary search
+    def cari_server(self, target):
+        data = []
+        for i in server_list:
+            data.append([i.nama, i.id, i.ip, i.status])
+        
         data.sort()
         left = 0
         right = len(data) - 1
         
-        print('Searching IP...')
+        print('Searching IP...\n')
         
         while left <= right:
             mid = (left + right) // 2
             
-            if data[mid].nama == target:
+            if data[mid][2] == target:
                 print('FOUND:')
-                print(f'Server_Name : {data.nama}')
-                print(f'Server_Id : {data.id}')
-                print(f'IP : {data.ip}')
-                print(f'Status : {data.status}')
+                print(f'Server_Name : {data[mid][0]}')
+                print(f'Server_Id   : {data[mid][1]}')
+                print(f'IP          : {data[mid][2]}')
+                print(f'Status      : {data[mid][3]}')
                 return
             
-            if data[mid].nama < target:
+            if data[mid][2] < target:
                 left = mid + 1
             else:
                 right = mid - 1
         else:
             print('IP Tidak Ditemukan!')
+
+if __name__ == '__main__':
+    ss = SearchingServer()
+    ss.cari_server()
