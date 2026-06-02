c3a0664e569178130a6fdfed6d5b5708cc373a5e
0Zeroiszero <abdullahaffandi0109@gmail.com>
2026-06-02 10:57:42 +0700

    Menambahkan komentar dan dokumentasi pada metode traversal dalam kelas BST

--- DSA/traversal_folder.py
+++ DSA/traversal_folder.py
@@ -1,24 +1,53 @@
+"""
+@author: PLEASE INI SIAPA YANG BUAT <<<<<<<<
+@modified: 02/06/2026 oleh Abdullah Affandi
+- kontrak abstraksi untuk traversal pada binary search tree
+- preorder, inorder, postorder
+- kalau mau buat traversal lain, buat method baru dengan abstraksi yang sama
+"""
+
+from abc import abstractmethod
+
 class BST:
     def __init__(self):
         pass
-
+    
+    @abstractmethod
     # root - left - right
     def preorder(self, node):
-        if node is not None:
-            print(node.data, end=' ')
-            self.preorder(node.left)
-            self.preorder(node.right)
+        """
+        Gunakan rekursi untuk menampilkan node.left dan node.right
+        """
+        # if node is not None:
+            # print(node.data, end=' ')
 
+            # self.preorder(node.left)
+            # self.preorder(node.right)
+        pass
+
+    @abstractmethod
     # left - root - right 
     def inorder(self, node):
-        if node is not None:
-            self.in_order(node.left)
-            print(node.data, end=' ')
-            self.in_order(node.right)
+        """
+        Gunakan rekursi untuk menampilkan node.left dan node.right
+        """
+        # if node is not None:
+            # self.in_order(node.left)
+            
+            # print(node.data, end=' ')
 
+            # self.in_order(node.right)
+        pass
+
+    @abstractmethod
     # left - right - root
     def postorder(self, node):
-        if node is not None:
-            self.postorder(node.left)
-            self.postorder(node.right)
-            print(node.data, end=' ')
+        """
+        Gunakan rekursi untuk menampilkan node.left dan node.right
+        """
+        # if node is not None:
+            # self.postorder(node.left)
+            # self.postorder(node.right)
+            
+            # print(node.data, end=' ')
+        pass
